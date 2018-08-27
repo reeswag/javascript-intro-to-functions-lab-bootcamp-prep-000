d97e33a4874db136f0178c3630264e3db904f52f
William Rees <reeswag@gmail.com>
2018-08-27 12:39:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
- console.log(string.toUpperCase())
-}function logWhisper(string) {
+ console.log(string.toUpperCase())}
+function logWhisper(string) {
  console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
   if string === string.toLowerCase()
-  return "I can't hear you!"
-  else
-  
-}
+  return "I can't hear you!"}
