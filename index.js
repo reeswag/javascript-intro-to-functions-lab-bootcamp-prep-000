17d1816f41e0ed883a92ed305ce1f026d33e2345
William Rees <reeswag@gmail.com>
2018-08-27 12:47:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase())
   {return "I can't hear you!"}
+  else if(string==="I love you, Grandma.")
+  {return "I love you, too."}
   else
   {return "YES INDEED!"}
 }
